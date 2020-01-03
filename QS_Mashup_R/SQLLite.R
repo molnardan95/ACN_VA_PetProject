bef07d380074f1c68a1e1b68cd61d3999a5d9300
Molnar <daniel.molnar@accenture.com>
2020-01-03 13:19:53 +0100

    Fixed and commented R file

--- QS_Mashup_R/SQLLite.R
+++ QS_Mashup_R/SQLLite.R
@@ -1,28 +1,31 @@
 #install.packages("RSQLite")
-
 library(DBI)
+
+# ESTABLISH CONNECTION WITH DB FILE
 con <- dbConnect(RSQLite::SQLite(), "nba_player_data_R.db")
 
 dbListTables(con)
 
 con
 
+# READ DATA FILES
+player_data <- read.csv(file="../data/player_data.csv", header=TRUE, sep=",")
+players <- read.csv("../data/Players.csv", header=TRUE, sep=",")
+season_stats <- read.csv("../data/Seasons_Stats.csv", header=TRUE, sep=",")
 
-player_data <- read.csv(file="../nba-players-stats/player_data.csv", header=TRUE, sep=",")
-players <- read.csv("../nba-players-stats/Players.csv", header=TRUE, sep=",")
-season_stats <- read.csv("../nba-players-stats/Seasons_Stats.csv", header=TRUE, sep=",")
-
-dbWriteTable(con, "player_data", player_data)
-dbWriteTable(con, "players", players)
-dbWriteTable(con, "season_stats", season_stats)
+# SAVE DATA FILES TO DB
+dbWriteTable(con, "player_data", player_data, overwrite = TRUE)
+dbWriteTable(con, "players", players, overwrite = TRUE)
+dbWriteTable(con, "season_stats", season_stats, overwrite = TRUE)
 
 
+# EXAMPLES FOR READING TABLE
 dbReadTable(con, "players")
 res <- dbSendQuery(con, "SELECT * FROM players WHERE height > 200")
 
 dbFetch(res)
-
 dbClearResult(res)
 
+# CONNECTION SHOULD BE CLOSED IN THE END
 dbDisconnect(con)
 
